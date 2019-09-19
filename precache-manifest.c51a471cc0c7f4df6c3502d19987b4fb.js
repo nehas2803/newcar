09648248db19525172a8580f94f2069dd783dc0a
nehas2803 <nehas2803@gmail.com>
2019-09-20 00:15:11 +0530

    Updates

---
+++ precache-manifest.c51a471cc0c7f4df6c3502d19987b4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd26d897e4e385cec9f3774272f446d",
+    "url": "/nehas2803/newcar/index.html"
+  },
+  {
+    "revision": "f43d65a786bcefb48e26",
+    "url": "/nehas2803/newcar/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cb63b95d2c1a3ae64b85",
+    "url": "/nehas2803/newcar/static/css/main.612ec0e7.chunk.css"
+  },
+  {
+    "revision": "f43d65a786bcefb48e26",
+    "url": "/nehas2803/newcar/static/js/2.7e03dd89.chunk.js"
+  },
+  {
+    "revision": "cb63b95d2c1a3ae64b85",
+    "url": "/nehas2803/newcar/static/js/main.bf68ed5b.chunk.js"
+  },
+  {
+    "revision": "3a3b319422bcc1ce3121",
+    "url": "/nehas2803/newcar/static/js/runtime~main.3226ff2b.js"
+  }
+]);
